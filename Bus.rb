08615d6c78eb5f0dbfe7bcccd3cca87f5887b5b8
Iain Tooley <iaintooley1@gmail.com>
2019-09-17 13:54:08 +0100

    added test for drive

---
+++ Bus.rb
@@ -0,0 +1,11 @@
+class Bus
+
+  def initialize(route, destination)
+    @route = route
+    @destination = destination
+    @passengers = []
+  end
+
+
+
+end
---
+++ specs/bus_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../Bus")
+require_relative("../Person")
+
+class TestBus < Minitest::Test
+
+  def setup()
+    @bus = Bus.new(22, "Ocean Terminal")
+  end
+
+  def test_that_bus_drives()
+    assert_equal("Brum Brum", @bus.drive())
+  end
+
+end
