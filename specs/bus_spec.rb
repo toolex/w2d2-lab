fd1e2f460bc02892fa9120ce571e816d8a329243
Iain Tooley <iaintooley1@gmail.com>
2019-09-17 14:31:23 +0100

    written test for picking up everyone from stop

--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -2,6 +2,7 @@ require("minitest/autorun")
 require("minitest/rg")
 require_relative("../Bus")
 require_relative("../Person")
+require_relative("../BusStop")
 
 class TestBus < Minitest::Test
 
@@ -9,6 +10,7 @@ class TestBus < Minitest::Test
     @bus = Bus.new(22, "Ocean Terminal")
     @person1 = Person.new("Matilda", 102)
     @person2 = Person.new("Bob", 4)
+    @bus_stop = BusStop.new("Castle Terrace", [@person1, @person2])
   end
 
   def test_that_bus_drives()
@@ -34,7 +36,13 @@ class TestBus < Minitest::Test
   def test_bus_emptied
     @bus.pick_up(@person1)
     @bus.empty()
-    assert_equal(0, @bus.count_passengers)
+    assert_equal(0, @bus.count_passengers())
+  end
+
+  def test_pick_up_everyone_from_stop
+    @bus.pick_up_from_stop(@bus_stop)
+    assert_equal(2, @bus.count_passengers())
+    assert_equal(0, @bus_stop.queue_length())
   end
 
 end
